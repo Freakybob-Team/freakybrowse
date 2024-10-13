91ec4a263cf1083682518825b459ccdeec739200
Squirrel <rylancarey2011@gmail.com>
2024-10-13 12:42:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,101 +2,82 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtWebEngineWidgets import *
-from PyQt5.QtPrintSupport import *
-import os
 import sys
-class MainWindow(QMainWindow):
 
+class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-
         self.browser = QWebEngineView()
-
-        self.browser.setUrl(QUrl("https://search.freakybob.site"))
-
+        self.browser.setUrl(QUrl("https://search.freakybob.site/chrome/newtab#gsc.tab=0"))
         self.browser.loadFinished.connect(self.update_title)
-
         self.setCentralWidget(self.browser)
 
         self.status = QStatusBar()
-
         self.setStatusBar(self.status)
 
         navtb = QToolBar("Navigation")
-
         self.addToolBar(navtb)
 
+       
         back_btn = QAction("Back", self)
-
-        back_btn.setStatusTip("Go back!11!1")
-
+        back_btn.setStatusTip("Go back")
         back_btn.triggered.connect(self.browser.back)
-
         navtb.addAction(back_btn)
 
+        
         next_btn = QAction("Forward", self)
-        next_btn.setStatusTip("Go forward!11!!")
-
+        next_btn.setStatusTip("Go forward")
         next_btn.triggered.connect(self.browser.forward)
         navtb.addAction(next_btn)
 
+       
         reload_btn = QAction("Reload", self)
-        reload_btn.setStatusTip("Reload!111!!")
-
+        reload_btn.setStatusTip("Reload the page")
         reload_btn.triggered.connect(self.browser.reload)
         navtb.addAction(reload_btn)
 
+        
         home_btn = QAction("Home", self)
-        home_btn.setStatusTip("Go home :3")
+        home_btn.setStatusTip("Go home")
+        home_btn.triggered.connect(self.navigate_home)  
         navtb.addAction(home_btn)
 
-        navtb.addSeparator
+        navtb.addSeparator()
 
+        
         self.urlbar = QLineEdit()
-
         self.urlbar.returnPressed.connect(self.navigate_to_url)
-
         navtb.addWidget(self.urlbar)
 
+        
         stop_btn = QAction("Stop", self)
-        stop_btn.setStatusTip("Stop Loading!111!")
-
+        stop_btn.setStatusTip("Stop loading the page")
         stop_btn.triggered.connect(self.browser.stop)
         navtb.addAction(stop_btn)
 
         self.show()
 
-
     def update_title(self):
         title = self.browser.page().title()
-        self.setWindowTitle("% s - FreakyBrowse, by the Freakybob Team." % title)
-
+        self.setWindowTitle(f"{title} - FreakyBrowse, by the Freakybob Team.")
+        self.update_urlbar(self.browser.url())
 
     def navigate_home(self):
-
-        self.browser.setUrl(QUrl("https://search.freakybob.site"))
-
+        self.browser.setUrl(QUrl("https://search.freakybob.site/chrome/newtab#gsc.tab=0"))
 
     def navigate_to_url(self):
-
         q = QUrl(self.urlbar.text())
-
         if q.scheme() == "":
-            q.setScheme("https")
-            self.browser.setUrl(q)
+            q.setScheme("https")  
+        self.browser.setUrl(q)
 
-def update_urlbar(self, q):
+    def update_urlbar(self, q):
+        self.urlbar.setText(q.toString())
+        self.urlbar.setCursorPosition(0)
 
-    self.urlbar.setText(q.toString())
-
-    self.urlbar.setCursorPosition(0)
-
-
-app = QApplication(sys.argv)
-
-app.setApplicationName("FreakyBrowse, by the Freakybob Team")
-
-window = MainWindow()
-
-app.exec_()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setApplicationName("FreakyBrowse, by the Freakybob Team")
+    window = MainWindow()
+    app.exec_()
