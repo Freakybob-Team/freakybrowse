2a7723e4eeff2de7e2fad261a3180e0e633a408a
Wish <100239673+wish13yt@users.noreply.github.com>
2025-01-02 14:50:44 -0600

    g

--- Code/updater/update_your_browse.py
+++ Code/updater/update_your_browse.py
@@ -1,9 +1,10 @@
 import requests
 from urllib.request import urlretrieve
-current_url = "https://github.com/Freakybob-Team/freakybrowse/releases/download/v2.2/2.2.zip"
+current_url = "https://github.com/Freakybob-Team/freakybrowse/releases/download/v2.2/2.2.zip" # change this upon release
 url = "https://github.com/Freakybob-Team/freakybrowse/blob/main/newest_release.txt?raw=true"
 res = requests.get(url)
-print(res.text)
+print("URL from Local: " + current_url)
+print("URL from Cloud: " + res.text)
 if (res.text == current_url):
     print("No new version has been found!")
 else:
