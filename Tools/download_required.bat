fe4043db626bab6ec93b2560f6aef128b468f9e5
Squirrel <squirrelhomebrew@gmail.com>
2025-01-05 20:18:56 -0800

    check if python is installed cause why not fr

--- Tools/download_required.bat
+++ Tools/download_required.bat
@@ -1,5 +1,14 @@
 @echo off
 cd ..
+
+python --version >nul 2>&1
+
+if %errorlevel% equ 0 (
+  echo good! you have python installed :fire:
+) else (
+  echo you can't run FreakyBrowse without Python. Go download it plz !!
+)
+
 pip install -r requirements.txt -q
 
 if %errorlevel% equ 0 (
